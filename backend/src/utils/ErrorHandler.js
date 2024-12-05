b4c8c2f65c4d9050affbd5dd4bcb36f015511b74
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-05 15:53:05 +0530

    Milestone 4

--- backend/src/utils/ErrorHandler.js
+++ backend/src/utils/ErrorHandler.js
@@ -1,4 +1,4 @@
-class ErrorHandler extends Error{
+export class ErrorHandler extends Error{
     constructor(message, statusCode) {
         super(message);
         Error.captureStackTrace(); //removes unnecesary files and shows the exact origin of the file
