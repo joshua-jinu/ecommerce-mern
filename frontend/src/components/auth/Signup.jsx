319887469cc73a87e29045afdbe5a6e136a0ec9a
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-12 13:20:36 +0530

    Added Form Validation

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
-import { useForm } from 'react-hook-form'
+import {Validate} from '../../validation.js'
 
 function Signup() {
-  const {register, handleSubmit, reset, formState} = useForm();
-  const {errors} = formState;
+  const [data, setData] = useState({
+    name: '',
+    email:'',
+    password:'',
+  })
+  const [error, setError] = useState('');
 
-  async function onSubmit(data){
-    console.log(data)
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+
+    setData((prev)=>({
+      ...prev,
+      [name]:value,
+    }));
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const NameV = Validate.validateName(data.name);
+    const EmailV = Validate.validateEmail(data.email);
+    const PassV = Validate.validatePass(data.password);
+
+
+    if(typeof NameV=='string' && NameV.length>1){
+      return setError(NameV);
+    }
+    if(typeof EmailV=='string' && EmailV.length>1){
+      return setError(EmailV);
+    }
+    if(typeof PassV=='string' && PassV.length>1){
+      return setError(PassV);
+    }
+
   }
 
   return (
@@ -18,8 +46,10 @@ function Signup() {
         </h2>
       </div>
 
+
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action="#" onSubmit={handleSubmit(onSubmit)} className="space-y-6">
+        {error}
+        <form action="#" onSubmit={handleSubmit} className="space-y-6">
           <div>
             <label htmlFor="name" className="block text-sm/6 font-medium text-gray-900">
               Name
@@ -31,24 +61,27 @@ function Signup() {
                 type="text"
                 autoComplete="name"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                {...register('name', {required: "this field is required"})}
-                />
+                value={data.name}
+                onChange={handleChange}
+              />
             </div>
           </div>
+
+
           <div>
             <label htmlFor="email" className="block text-sm/6 font-medium text-gray-900">
               Email address
             </label>
             <div className="mt-2">
-              {errors.email?.message}
               <input
                 id="email"
                 name="email"
                 type="email"
                 autoComplete="email"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                {...register('email', {required: "this field is required"})}
-                />
+                value={data.email}
+                onChange={handleChange}
+              />
             </div>
           </div>
 
@@ -59,33 +92,32 @@ function Signup() {
               </label>
             </div>
             <div className="mt-2">
-              {errors.password?.message}
               <input
                 id="password"
                 name="password"
                 type="password"
                 autoComplete="new password"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                {...register('password', {required: "this field is required"})}
-                />
+                value={data.password}
+                onChange={handleChange}
+              />
             </div>
           </div>
+
           <div>
             <div className="flex items-center justify-between">
-              <label htmlFor="confirm-password" className="block text-sm/6 font-medium text-gray-900">
-                Confirm Password
+              <label htmlFor="password" className="block text-sm/6 font-medium text-gray-900">
+                Upload Profile Picture
               </label>
             </div>
             <div className="mt-2">
-              {errors.confirmPassword?.message}
               <input
-                id="confirm-password"
-                name="confirm-password"
-                type="password"
-                autoComplete="confirm password"
+                id="upload"
+                name="upload"
+                type="file"
+                accept=".jpg, .jpeg, .png"
                 className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
-                {...register('confirmPassword', {required: "this field is required"})}
-              />
+                />
             </div>
           </div>
 
