816cd17665f73fd5fca96b32f0fb812f71240d40
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-17 14:17:09 +0530

    Milestone 7

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,6 +2,7 @@ import User from '../models/user.model.js'
 import { ErrorHandler } from '../utils/ErrorHandler.js';
 import { transporter } from '../utils/sendMail.js';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs'
 
 const generateToken = (data) => {
     const token = jwt.sign({name: data.name, email: data.email, password: data.password}, process.env.SECRET_KEY);
@@ -16,21 +17,6 @@ const verifyUser = (token)=>{
         return false;
     }
 }
-
-export const verifyUserController = async (req, res) =>{
-    const {token} = req.params;
-    try {
-        if(verifyUser(token)){
-            //stores the token in the cookie
-            return res.status(200).cookie('token', token).json({token, success: true});
-        }
-        return res.status(403).send({message: 'token expired'});
-        
-    } catch (error) {
-        return res.status(403).send({message: error.message});
-    }
-}
-
 
 export const createUser = async (req, res) => {
     const {name, email, password} = req.body;
@@ -70,4 +56,79 @@ export const createUser = async (req, res) => {
     await newUser.save();
 
     return res.send("New User Created")
+}
+
+export const verifyUserController = async (req, res) =>{
+    const {token} = req.params;
+    try {
+        if(verifyUser(token)){
+            //stores the token in the cookie
+            return res.status(200).cookie('token', token).json({token, success: true});
+        }
+        return res.status(403).send({message: 'token expired'});
+        
+    } catch (error) {
+        return res.status(403).send({message: error.message});
+    }
+}
+
+export const signup = async (req, res) => {
+    const {name, email, password} = req.body;
+
+    if(!name || !email || !password){
+        console.log("data not provided")
+        return res.status(204).send({success: false, message: "Data not present"})
+    }
+
+    try{
+        const userExists = await User.findOne({email: email});
+        if(userExists){
+            const error =  new ErrorHandler(`user already exists`, 400);
+            return res.status(403).send({success:false, message: error.message, status:error.statusCode})
+        }
+
+        bcrypt.hash(password, 10, async function(err, hash){
+            try{
+                if(err){
+                    return res.status(403).send({message: 'Please Enter'})
+                }
+                await new User.create({
+                    name: name,
+                    email,
+                    password: hash,
+                })
+                return res.status(201).send({success: true, message: "User Created Successfully"});
+            }catch(err){
+                return res.status(500).send({success: false, message: "User Created Successfully"});
+            }
+        });
+    }catch(err){
+        return res.status(500).send({success: false, message: `Server error in signup: ${err.message}`})
+    };
+}
+
+export const login = async (req, res)=>{
+    const {email, password} = req.body;
+    try{
+        const userExists = await User.findOne({email: email});
+
+        bcrypt.compare(password, userExists.password, function (err, res){
+            if(err)
+                res.status(403).send({success:false, message: err.message});
+            let data = {
+                id: userExists._id,
+                email,
+                password: userExists.password,
+            };
+    
+            const token = generateToken(data); 
+
+            return res
+                .status(200)
+                .cookie('token', token)
+                .send({success: true, message: "User logged in successfully.."})
+        });
+    }catch(err){
+        return res.status(403).send({success: false, message: err.message});
+    }
 }
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, verifyUserController } from "../controllers/user.controller.js";
+import { createUser, login, signup, verifyUserController } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.js";
 
 const router = Router();
@@ -7,5 +7,7 @@ const router = Router();
 router.post("/create-user", upload.single("file"), createUser);
 router.get("/authentication/:token", verifyUserController); //url to verify users
 
+router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
