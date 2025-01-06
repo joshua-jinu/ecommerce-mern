4b24152fab82e90d18b5ae83557f823d8f7dafe3
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-01-06 15:58:25 +0530

    Updated README.md

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,12 +3,7 @@ import Card from '../components/Card'
 import axios from 'axios';
 
 function Home() {
-    const [products, setData] = useState([{
-        title: "Nike Air MX Super 2500 - Red",
-        images: ["https://images.unsplash.com/photo-1600185365483-26d7a4cc7519?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"],
-        discountedPrice: 499,
-        price: 699
-    }]);
+    const [products, setData] = useState([]);
  
     useEffect(()=>{
         const fetchProducts = async () =>{
