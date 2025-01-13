ef82a52d814ec9bdc25c3b2b958384641f0b7404
Joshua Jinu <joshuajinu007@gmail.com>
2025-01-13 15:26:41 +0530

    Revert "Milestone 15"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,9 @@
 import { Routes, Route } from 'react-router-dom';
 import Router from './routes/Router';
-import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <>
-      <Navbar/>
-      <Router/>
-    </>
+    <Router/>
   )
 }
 
