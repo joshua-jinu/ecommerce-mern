f81c60fadd19b274555843f126455ee3bbfe3127
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-02-03 15:07:17 +0530

    milestone 26

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -1,4 +1,5 @@
 import orderModel from "../models/order.model";
+import userModel from "../models/user.model";
 
 export async function confirmOrder(req, res) {
   const userId = req.userId;
@@ -30,5 +31,33 @@ export async function confirmOrder(req, res) {
     return res
       .status(500)
       .send({ message: "Internal Server Error", success: false });
-  }
+    }
+}
+
+export async function getOrders(req, res){
+    const userId = req.userId;
+    
+    try {
+        if (!mongoose.Types.ObjectId.isValid(userId)) {
+            return res
+              .status(400)
+              .send({ message: "invalid user id", success: false });
+            }
+        const checkUser = await userModel.findOne({_id: userId});
+        if(!checkUser){
+            return res
+              .status(400)
+              .send({ message: "User not found, Sign up now", success: false });
+
+        }
+
+        const orders = orderModel.find({user: userId});
+
+        return res.status(200).send({message:"user orders fetched successfully", success: true, orders})
+    } catch (error) {
+        console.log('error in fetching orders');
+        return res
+          .status(500)
+          .send({ message: "Internal Server Error", success: false });
+    }
 }
--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { Router } from "express";
-import { verifyUser } from "../middlewares/jwtverify";
-import { confirmOrder } from "../controllers/order.controller";
+import { verifyUser } from "../middlewares/jwtverify.js";
+import { confirmOrder, getOrders } from "../controllers/order.controller.js";
 
 const router = Router();
 
 router.post("/confirm-order", verifyUser, confirmOrder);
+router.get("/user-orders-data", verifyUser, getOrders);
 
 export default router;
